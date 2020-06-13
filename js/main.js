74c53a501f1f05d20068cab466b59d8786d63891
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-06-13 22:53:13 +0900

    v-on create
    v-onでイベントを設定

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task 1',
         'task 2',
         'task 3'
       ]
+    },
+    methods: {
+      addItem: function(){
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
     }
   });
 
